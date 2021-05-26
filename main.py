018943ad4d9dd7a359ff1c7db6595cb4005bc33e
Corty-Plato <corentin.chanet@gmail.com>
2021-05-26 14:42:02 +0200

    Success using threading

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 import time
 import random
 from utils.output import raw_to_csv, clean_to_csv, cleaning
-from utils.fetching import fetching_urls, fetching_data, select_driver
+from utils.fetching import fetching_urls, fetching_data, FetchThread
 from selenium import webdriver
 from threading import Thread
+from bs4 import BeautifulSoup
+import requests
+import json
 
+threads = list()
 
 def main():
 
     driver_name = input("Select webdriver: safari, chrome or firefox")
 
-    threads = list()
-
     i=1
     while i<3:
         url = f"https://www.immoweb.be/en/search/house-and-apartment/for-sale?countries=BE&page={i}&orderBy=newest"
         urls = fetching_urls(url, driver_name)
-        t = Thread(name='Test {}'.format(i), target=fetching_data, args=(urls,))
+        t = FetchThread(name='Test_{}'.format(i), target=fetching_data, args=(urls,))
         t.start() # fetching_data(urls)
         threads.append(t)
         time.sleep(0.5 + random.random())
-        print(i)
         i += 1
         time.sleep(0.5 + random.random())
+
+
 
     #raw_to_csv()
 
