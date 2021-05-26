d6cd7b68b6fd651f1e17fef7023c421c5e58bb8f
Corty-Plato <corentin.chanet@gmail.com>
2021-05-26 10:10:56 +0200

    Start working on the main.py

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import time
+import random
 from utils.output import raw_to_csv, clean_to_csv, cleaning
 from utils.fetching import fetching_data, fetching_urls
 from selenium import webdriver
-
+from threading import Thread
 
 
 def main():
     driver = webdriver.Safari()
 
-    fetching_urls()
+    i=1
+    while True:
+        url = f"https://www.immoweb.be/en/search/house-and-apartment/for-sale?countries=BE&page={i}&orderBy=newest"
+        urls = fetching_urls(url, driver)
+        time.sleep(0.5 + random.random())
 
-    time.sleep(1)
 
     fetching_data()
 
