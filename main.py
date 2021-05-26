5b22910cf34924f050c56617a338acce6db998a6
Corty-Plato <corentin.chanet@gmail.com>
2021-05-26 15:50:38 +0200

    Fetching houses and apartments separately - otherwise, not enough records (goal: 10 000)

--- main.py
+++ main.py
@@ -14,15 +14,16 @@ def main():
 
     driver_name = input("Select webdriver: safari, chrome or firefox")
 
-    i=1
-    while i<3:
-        url = f"https://www.immoweb.be/en/search/house-and-apartment/for-sale?countries=BE&page={i}&orderBy=newest"
-        urls = fetching_urls(url, driver_name)
-        t = FetchThread(name='Test_{}'.format(i), target=fetching_data, args=(urls,))
-        t.start() # fetching_data(urls)
-        threads.append(t)
-        time.sleep(0.5 + random.random())
-        i += 1
+    for property_type in ("house", "apartment"):
+        i=1
+        while i<3:
+            url = f"https://www.immoweb.be/en/search/{property_type}/for-sale?countries=BE&page={i}&orderBy=newest"
+            urls = fetching_urls(url, driver_name)
+            t = FetchThread(name='Test_{}'.format(i), target=fetching_data, args=(urls,))
+            t.start() # fetching_data(urls)
+            threads.append(t)
+            time.sleep(0.5 + random.random())
+            i += 1
 
     print(_threads_to_dict(threads))
 
