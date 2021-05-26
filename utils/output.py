cb5d1855386bff55031234ce4965a00da2b8898f
Corty-Plato <corentin.chanet@gmail.com>
2021-05-26 15:08:14 +0200

    Complete first version of threads_to_dict function

--- utils/output.py
+++ utils/output.py
@@ -1,10 +1,16 @@
 import time
 
-def threads_to_dict(threads):
+def _threads_to_dict(threads):
+    items = []
     for thread in threads:
+        fields = ("Type", "Subtype", "Price", "Number of rooms", "Area", "Fully equipped kitchen",
+                  "Furnished", "Open Fire", "Terrace", "Plot size", "Number of facades", "Swimming pool", "State")
         while thread.is_alive():
-            time.sleep(4)
-            pass
+            time.sleep(3)
+
+        item = {field: value for field, value in zip(fields, thread.data)}
+        items.append(item)
+    return items
 
 def raw_to_csv():
     pass
