f3a3e2594cbfe1ebbc3d24b64c074c1d5886949b
Corty-Plato <corentin.chanet@gmail.com>
2021-05-26 12:15:08 +0200

    Starting the threading on fetching function

--- main.py
+++ main.py
@@ -1,30 +1,36 @@
 import time
 import random
 from utils.output import raw_to_csv, clean_to_csv, cleaning
-from utils.fetching import fetching_data, fetching_urls
+from utils.fetching import fetching_urls, fetching_data, select_driver
 from selenium import webdriver
 from threading import Thread
 
 
 def main():
-    driver = webdriver.Safari()
+
+    driver_name = input("Select webdriver: safari, chrome or firefox")
+
+    threads = list()
 
     i=1
-    while True:
+    while i<3:
         url = f"https://www.immoweb.be/en/search/house-and-apartment/for-sale?countries=BE&page={i}&orderBy=newest"
-        urls = fetching_urls(url, driver)
+        urls = fetching_urls(url, driver_name)
+        t = Thread(name='Test {}'.format(i), target=fetching_data, args=(urls,))
+        t.start() # fetching_data(urls)
+        threads.append(t)
+        time.sleep(0.5 + random.random())
+        print(i)
+        i += 1
         time.sleep(0.5 + random.random())
 
+    #raw_to_csv()
 
-    fetching_data()
+    #cleaning()
 
-    raw_to_csv()
+    #clean_to_csv()
 
-    cleaning()
-
-    clean_to_csv()
-
-    driver.close()
+    #driver.close()
 
     pass
 
--- utils/fetching.py
+++ utils/fetching.py
@@ -3,29 +3,51 @@ import random
 from selenium import webdriver
 from threading import Thread
 
-def fetching_urls(url: str, driver) -> list:
+def select_driver(name):
+    if name.lower() == 'safari':
+        return webdriver.Safari()
+    elif name.lower() == 'chrome':
+        return webdriver.Chrome()
+    else:
+        return webdriver.Firefox()
 
+def fetching_urls(url: str, driver_name) -> list:
+    driver = select_driver(driver_name)
     driver.get(url)
 
     urls = []
     container = driver.find_element_by_id("main-content")
     for li in container.find_elements_by_tag_name("a"):
-        urls.append(li.get_attribute("href"))
+        try:
+            urls.append(str(li.get_attribute("href")))
+        except:
+            print("No href found - passing over element")
+            continue
+
+    driver.close()
 
     return urls
 
-def fetching_data(urls: list, driver:str) -> dict:
-    pass
+def fetching_data(urls:list):
+    driver = webdriver.Safari()
+    for url in urls:
+        driver.get(url)
+        time.sleep(2)
+        print("DATA FETCHED! : " + str(url))
+    driver.close()
 
-class FetchThread(Thread):
-    def __init__(self, driver, urls):
-        Thread.__init__(self)
-        self.urls = urls
-        self.data = []
-        self.drivers = {"safari": webdriver.Safari(), "chrome": webdriver.Chrome(), "firefox": webdriver.Firefox()}
-        self.driver = self.drivers[driver.lower()]
 
-    def run(self):
-        for url in self.urls:
-            self.data.append(fetching_data(url))
-            time.sleep(1+random.random())
+# class FetchThread(Thread):
+#     def __init__(self, driver, urls):
+#         Thread.__init__(self)
+#         self.urls = urls
+#         self.data = []
+#         self.driver = driver
+#
+#     def run(self):
+#         for url in self.urls:
+#             # code to fetch data from script
+#             print('THREAD STARTED')
+#             time.sleep(1+random.random())
+#
+#         self.driver.close()
