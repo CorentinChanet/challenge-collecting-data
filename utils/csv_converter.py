a1e8ce8b409c3066618e75e896e036cb2c0cc099
Corty-Plato <corentin.chanet@gmail.com>
2021-05-27 16:53:19 +0200

    Rename and document raw_to_csv function

--- utils/csv_converter.py
+++ utils/csv_converter.py
@@ -2,20 +2,23 @@ import pandas as pd
 import os.path
 from typing import List, Dict, Union, Optional
 
-def raw_to_csv(list_of_properties: List[Dict[str, Optional[Union[str,float,int]]]]) -> pd.DataFrame:
+def raw_to_csv(list_of_properties: List[Dict[str, Optional[Union[str,float,int]]]]) -> None:
     '''
-    a function that will create a CSV file with the provided list of properties ( estates )
-    user needs to specify file name, the file is created in the root directory
+    A function that will create two CSV files with the provided list of properties ( estates ).
+    User needs to specify each file name (raw and clean), the file is created in the same directory as main.py.
 
     :param list_of_properties: a list of dictionaries containing the properties
     '''
-    print("choose file name:")
+    print("choose raw_table file name:")
     file_name = input()
-    table_of_properties = pd.DataFrame(list_of_properties)
-    table_of_properties.to_csv(f"{file_name}.csv")
+    raw_table = pd.DataFrame(list_of_properties)
+    raw_table.to_csv(f"{file_name}.csv")
 
-    return table_of_properties
+    print("choose clean_table file name:")
+    file_name = input()
+    clean_table = _cleaner(raw_table)
+    clean_table.to_csv(f"{file_name}.csv")
 
-def clean_to_csv(table: pd.DataFrame) -> None:
+def _cleaner(table: pd.DataFrame) -> pd.DataFrame:
     # Function that will clean the panda table and create a corresponding csv file
     pass
