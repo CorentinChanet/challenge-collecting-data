79cd057a5d5e5322bfe95abade33d48ed470dd8c
hakan ergin <hakan.ergin@gmail.com>
2021-05-27 12:06:41 +0200

    deleted comments

--- utils/csv_converter.py
+++ utils/csv_converter.py
@@ -16,10 +16,3 @@ def convert_to_csv(list_of_properties: List[Dict[str, Optional[Union[str,float,i
     table_of_properties = pd.DataFrame(list_of_properties)
     table_of_properties.to_csv(os.path.join(parent_path, f"{file_name}.csv"))
 
-# test_data = [{"id":1,"first_name":"Lisetta","last_name":"Rowett","email":"lrowett0@ed.gov","gender":"Genderfluid"},
-#     {"id":2,"first_name":"Swen","last_name":"O'Noulane","email":"sonoulane1@clickbank.net","gender":"Male"},
-#     {"id":3,"first_name":"Bertrando","last_name":"Cancutt","email":"bcancutt2@baidu.com","gender":"Polygender"},
-#     {"id":4,"first_name":"Stacee","last_name":"Maeer","email":"smaeer3@paginegialle.it","gender":"Male"},
-#     {"id":5,"first_name":"Bernhard","last_name":"Mingame","email":"bmingame4@myspace.com","gender":"Bigender"}]
-
-# convert_to_csv(test_data)
